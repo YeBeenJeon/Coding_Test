bb9df6055b208350271f844f2fdc457b3a731c1d
YeBeen <105630981+YeBeenJeon@users.noreply.github.com>
2023-09-07 09:38:45 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (50.19%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int addDigits(int num) {
         
+        // Digital Root
         int dr;
-        
+        // 자신보다 작은 수 중, 가장 큰 9의 배수에서 몇 번째인지
         dr = num - 9 * floor((num-1)/9);
-        
-        cout << dr << "\n";
-        
+                
         return dr;
         
     }
